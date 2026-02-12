b2692b4e2f7f616686322c56a2217d028eb354e9
Jeff Zhang <jeff.j.zhang@shopify.com>
2026-02-12 14:43:34 -0500

    output to stderr instead

--- tool/zjit_diff.rb
+++ tool/zjit_diff.rb
@@ -22,7 +22,7 @@ class CommandRunner
   def cmd(*args, quiet: nil)
     quiet = @quiet if quiet.nil?
     options = { exception: true }
-    options = options.merge(out: File::NULL) if quiet
+    options = options.merge(out: quiet ? File::NULL : $stderr)
     system(*args, **options)
   end
 end
